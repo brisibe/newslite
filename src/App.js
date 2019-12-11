971e2a84d92be7daf1f8d67dec87982c5f67bea3
jbrisibe10 <43288228+jbrisibe10@users.noreply.github.com>
2019-12-11 21:07:32 +0100

    worked on the component styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import "./App.css";
+import NewsCard from "./components/newsCard";
 
 function App() {
   const date = new Date();
+
   return (
     <div className="App">
       <div className="navbar">
@@ -11,36 +14,53 @@ function App() {
         </div>
       </div>
 
-      <div className="main row">
-        <div className="menu col-2">
-          <ul>
-            <li>World</li>
-            <li>Nigeria</li>
-            <li>Sports</li>
-            <li>Fashion</li>
-          </ul>
-        </div>
-        <div className="info col-10">
-          <img src="./asset.svg" alt="wave"></img>
-          <p>
-            A graphic representation of data abstracted from the Chinese
-            program’s thrust, a worrying impression of solid fluidity, as though
-            the shards of a broken mirror bent and elongated as they rotated,
-            but it never told the correct time. The Sprawl was a square of faint
-            light. Images formed and reformed: a flickering montage of the
-            Sprawl’s towers and ragged Fuller domes, dim figures moving toward
-            him in the Japanese night like live wire voodoo and he’d cry for it,
-            cry in his jacket pocket. She peered at the clinic, Molly took him
-            to the Tank War, mouth touched with hot gold as a gliding cursor
-            struck sparks from the wall of a skyscraper canyon. He’d waited in
-            the shade beneath a bridge or overpass. Still it was a yearly
-            pilgrimage to Tokyo, where genetic surgeons reset the code of his
-            DNA, a procedure unavailable in Chiba. Case felt the edge of the
-            Flatline as a construct, a hardwired ROM cassette replicating a dead
-            man’s skills, obsessions, kneejerk responses. Light from a service
-            hatch at the rear of the deck sting his palm as he made his way down
-            Shiga from the sushi stall he cradled it in his jacket pocket.
-          </p>
+      <div className="main">
+        <Router>
+          <div className="menu-tab">
+            <ul className="container">
+              <Link>
+                <li>world</li>
+              </Link>
+              <Link>
+                <li>Nigerian</li>
+              </Link>
+              <Link>
+                <li>Sports</li>
+              </Link>
+            </ul>
+          </div>
+        </Router>
+        <div className="main-info ">
+          <div className="info container row">
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            {/* <p>
+              A graphic representation of data abstracted from the Chinese
+              program’s thrust, a worrying impression of solid fluidity, as
+              though the shards of a broken mirror bent and elongated as they
+              rotated, but it never told the correct time. The Sprawl was a
+              square of faint light. Images formed and reformed: a flickering
+              montage of the Sprawl’s towers and ragged Fuller domes, dim
+              figures moving toward him in the Japanese night like live wire
+              voodoo and he’d cry for it, cry in his jacket pocket. She peered
+              at the clinic, Molly took him to the Tank War, mouth touched with
+              hot gold as a gliding cursor struck sparks from the wall of a
+              skyscraper canyon. He’d waited in the shade beneath a bridge or
+              overpass. Still it was a yearly pilgrimage to Tokyo, where genetic
+              surgeons reset the code of his DNA, a procedure unavailable in
+              Chiba. Case felt the edge of the Flatline as a construct, a
+              hardwired ROM cassette replicating a dead man’s skills,
+              obsessions, kneejerk responses. Light from a service hatch at the
+              rear of the deck sting his palm as he made his way down Shiga from
+              the sushi stall he cradled it in his jacket pocket.
+            </p> */}
+          </div>
         </div>
       </div>
 
---
+++ src/components/newsCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+export default class newsCard extends Component {
+  render() {
+    return (
+      <div className="mycard col-sm">
+        <img
+          className="mycard-img-top"
+          src="https://via.placeholder.com/100"
+        ></img>
+
+        <h5 className="mycard-title">Card title has declared war on drugs</h5>
+        <div className="date-source">
+          <p>BBC</p> <span>2018</span>
+        </div>
+        <p className="mycard-text">
+          Some quick example text to build on the card title and j af aj fa jfr
+          rrj rfj iron fnjvkjrn make up the bulk of the card's content.
+        </p>
+      </div>
+    );
+  }
+}
