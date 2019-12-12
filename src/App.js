ee97704239a1905415a41a3dfcfb181a555faec5
jbrisibe10 <43288228+jbrisibe10@users.noreply.github.com>
2019-12-12 15:36:05 +0100

    axios

--- src/App.js
+++ src/App.js
@@ -7,11 +7,16 @@ import axios from "axios";
 function App() {
   const date = new Date();
 
-  const data = axios
+  const info = axios
     .get(
-      "https://newsapi.org/v2/top-headlines?country=us&apiKey=b1f502747b7f432fae5be42e37ab32bd"
+      `https://newsapi.org/v2/top-headlines?country=us&apiKey=b1f502747b7f432fae5be42e37ab32bd`
     )
-    .then(res => console.log(res));
+    .then(res => {
+      const data = res.data.articles;
+      const { title, content, url, urlToimage, source } = data[0];
+      console.log(url);
+    })
+    .catch(err => console.log(err));
 
   return (
     <div className="App">
@@ -38,28 +43,7 @@ function App() {
         </div>
         {/* </Router> */}
         <div className="main-info ">
-          <div className="info container row">
-            {/* <p>
-              A graphic representation of data abstracted from the Chinese
-              program’s thrust, a worrying impression of solid fluidity, as
-              though the shards of a broken mirror bent and elongated as they
-              rotated, but it never told the correct time. The Sprawl was a
-              square of faint light. Images formed and reformed: a flickering
-              montage of the Sprawl’s towers and ragged Fuller domes, dim
-              figures moving toward him in the Japanese night like live wire
-              voodoo and he’d cry for it, cry in his jacket pocket. She peered
-              at the clinic, Molly took him to the Tank War, mouth touched with
-              hot gold as a gliding cursor struck sparks from the wall of a
-              skyscraper canyon. He’d waited in the shade beneath a bridge or
-              overpass. Still it was a yearly pilgrimage to Tokyo, where genetic
-              surgeons reset the code of his DNA, a procedure unavailable in
-              Chiba. Case felt the edge of the Flatline as a construct, a
-              hardwired ROM cassette replicating a dead man’s skills,
-              obsessions, kneejerk responses. Light from a service hatch at the
-              rear of the deck sting his palm as he made his way down Shiga from
-              the sushi stall he cradled it in his jacket pocket.
-            </p> */}
-          </div>
+          <div className="info container row"></div>
         </div>
       </div>
 
